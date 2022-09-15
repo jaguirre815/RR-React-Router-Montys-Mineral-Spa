da624f8633de62aea4995476d55f0e18f03a9d86
Joel Aguirre <joelaguirre@Joels-MacBook-Pro.local>
2022-09-14 21:09:39 -0500

    pushed changes

--- react-router/src/components/Packages.js
+++ react-router/src/components/Packages.js
@@ -1,12 +1,18 @@
 
 
-export default function Packages() {
+export default function Packages({ packages}) {
+    const display = packages.map((p, i) => {
+        return <li key={i}> {p}</li>
+    })
 
     return (
         <div>
             <div className="packages">
                 <div className="packagesHeader">
                     <h1>Our Packages</h1>
+                    <ul>
+                        {display}
+                    </ul>
                 </div>
                 
             </div>
